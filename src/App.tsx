342293088beb877b59b9d5ffa3751e2002fdf060
Estate Nest Capital Inc <hello@estatenest.capital>
2026-04-02 01:24:43 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Index } from "./pages/Index";
+import BuilderProfile from "./pages/BuilderProfile";
+import InvestorRelations from "./pages/InvestorRelations";
+import Careers from "./pages/Careers";
+import ManagementLogin from "./pages/ManagementLogin";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/builder-profile" element={<BuilderProfile />} />
+        <Route path="/investor-relations" element={<InvestorRelations />} />
+        <Route path="/careers" element={<Careers />} />
+        <Route path="/management-login" element={<ManagementLogin />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
