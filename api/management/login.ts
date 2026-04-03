61bff8e400ee85b419c2e9ce026b8b7654e766c3
Estate Nest Capital Inc <hello@estatenest.capital>
2026-04-03 14:57:49 -0600

    Update login.ts

--- api/management/login.ts
+++ api/management/login.ts
@@ -1,4 +1,3 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { buildSessionCookie, createSessionToken } from "../_lib/auth.js";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
